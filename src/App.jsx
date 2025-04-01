d1957d00d4da6ac601ed2ac695f5c9ae8862d8f2
cntencra <cn10cra@gmail.com>
2025-04-01 10:01:16 +0100

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Articles from './components/Articles';
 
 function App() {
 
   return (
     <>
+    <header>
+      <Header></Header>
+      <Navbar></Navbar>
+    </header>
+    <main>
+      <Routes>
+        <Route path = '/'/>
+        <Route path = '/article/:article_id'/>
+        <Route path = '/articles' element={<Articles />}/>
+        <Route path = '/articles/:topic'/>
+        <Route path = '/user'/>
+      </Routes>
+    </main>
     </>
   )
 }
