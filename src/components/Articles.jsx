385b58556cbf834b9d3c58ab67161c4e672200bc
cntencra <cn10cra@gmail.com>
2025-04-01 11:24:57 +0100

    Articles updated with useAPIFunction

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,31 +1,15 @@
 import { useEffect, useState } from "react"
 import { getArticles } from "../api"
 import ListArticle from "./ListArticle";
+import useApiRequest from "../useApiRequest";
 
  const Articles = () => {
 
-    const [articles, setArticles] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false)
+    const {data:articles, isLoading, error } = useApiRequest(getArticles);
 
-    useEffect(() => {
-        setIsLoading(true);
-        setIsError(false);
+    if (error) return <p>Error {error.msg}</p>
 
-        getArticles().then((articles) => {
-            setArticles(articles)
-        })
-        .catch(() => {
-            setIsError(true)
-        })
-        .finally(()=> {
-            setIsLoading(false)
-        })
-    },[])
-
-    if (isError) return <p>Error</p>
-
-    if (isLoading) return <p>Loading...</p>
+    if (isLoading) return <p>Loading... </p>
     
     return (
         <div id="articles-container">
