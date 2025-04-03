3eeaa7a3a770dea73866296f949f72d5bb09555e
cntencra <cn10cra@gmail.com>
2025-04-03 12:35:14 +0100

    feat: delete comment component

---
+++ src/components/DeleteComment.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { deleteComment } from "../api"
+
+const DeleteComment = ({commentId, onCommentDelete}) => {
+
+    
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const removeComment = ( commentId) => {
+   
+        setIsLoading(true);
+         deleteComment(commentId).then(()=>{
+            setError(null);
+            onCommentDelete(commentId);
+         })
+         .catch(()=> {
+            setError(true);
+         })
+         .finally(()=>{
+            setIsLoading(false)
+         })
+    }
+
+    const resetError = () => {
+        setError(null)
+        setIsLoading(false)
+    }
+
+
+    if (error) {
+        return (
+            <div id="delete-comment-unsuccessful">
+                <p>Your Delete was unsuccessful!</p>
+                <button onClick={resetError} id="delete-error-button">OK</button>
+            </div>
+        )
+    }
+
+    if (isLoading) {
+        return (
+            <div id="delete-comment-loading">
+                <p>Trying to delete...</p>
+            </div>
+        )
+    }
+
+    return (
+    <button className="comment-delete" onClick={() => removeComment( commentId)}>
+        <img src="../../src/assets/trash-delete-bin-svgrepo-com.svg" alt="Delete comment" />
+    </button>
+    )
+
+}
+
+export default DeleteComment
