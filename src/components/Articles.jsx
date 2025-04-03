e7becf1aba5652e4a4ce5f45fc7cd49dacacbb0e
cntencra <cn10cra@gmail.com>
2025-04-03 16:05:42 +0100

    Added topic useParams to Articles component

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -2,16 +2,24 @@ import '../css/articles.css'
 import { getArticles } from "../api"
 import ListArticle from "./ListArticle";
 import useApiRequest from "../useApiRequest";
+import { useParams } from 'react-router-dom';
 
  const Articles = () => {
 
-    const {data:articles, isLoading, error } = useApiRequest(getArticles);
+    const { topic } = useParams()
 
+    const {data:articles, isLoading, error } = useApiRequest(getArticles, topic);
     if (error) return <p>Error {error.msg}</p>
 
     if (isLoading) return <p>Loading... </p>
     
     return (
+        <>
+            {topic ? 
+                <h2 id="articles-title">{topic.slice(0,1).toUpperCase() + topic.slice(1).toLowerCase()}</h2>
+            :
+                <h2 id="articles-title">All Articles</h2>
+            }
             <ul id="articles-list">
             {articles.map((article)=> {
                 return (
@@ -21,6 +29,7 @@ import useApiRequest from "../useApiRequest";
                 )
             })}
             </ul>
+        </>
     )
  }
 
