b51dddd49376b60f14e2d93014803d91a1a42e34
cntencra <cn10cra@gmail.com>
2025-04-03 16:04:05 +0100

    feat: added dropdown to navbar with getTopics request

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,53 @@
-import { Link } from "react-router-dom"
+import { Link, Links } from "react-router-dom"
+import useApiRequest from "../useApiRequest";
+import { useState } from "react";
+import { getTopics } from "../api";
 
 const Navbar = () => {
+
+    const {data:topics, isLoading, error } = useApiRequest(getTopics)
+
     return (
         <nav>
             <Link to ="/articles">
                 <div id="articles-nav" className="nav-div"><h2>Articles</h2></div>
             </Link>
-            <div id="topics-nav-dropdown" className="nav-div"><h2>Topics</h2></div>
+            
+            <div className="topics-dropdown">
+
+                <div className="nav-div">
+                    <h2>Topics</h2>
+                </div>
+
+            {
+            isLoading ?
+            <ul className="topics-dropdown-content">
+                <li className="topic-dropdown">
+                    Loading topics ...
+                </li>
+            </ul>
+            :
+            error ?
+            <ul className="topics-dropdown-content">
+                <li className="topic-dropdown">
+                    Topics failed to load
+                </li>
+            </ul>
+            :
+            <ul className="topics-dropdown-content">
+                {topics.map((topic)=> {
+                    return (
+                    <li key={topic.slug} className="topic-list-item">
+                        <Link to={`/articles/${topic.slug}`} >
+                            {topic.slug.slice(0,1).toUpperCase() + topic.slug.slice(1).toLowerCase()}
+                        </Link>
+                    </li>
+                    )
+                })}
+            </ul> 
+            }
+            </div>
+
         </nav>
     )
 
