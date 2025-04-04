2120e53f300ec3c1c5be9d138503cc817c5e15f8
cntencra <cn10cra@gmail.com>
2025-04-04 13:58:08 +0100

    feat: ErrorPage component for bad urls

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import '../css/error.css'
+
+const ErrorPage = () => {
+    return (
+    <div className="error-page-container">
+        <p>How did you get here?</p>
+        <p>Go somewhere else!</p>
+        <p>Click a link at the top and stop typing in URLs yourself</p>
+        <p>this is not 2005</p>
+    </div>
+    )
+} 
+
+export default ErrorPage
