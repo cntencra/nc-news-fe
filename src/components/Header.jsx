157c045c9ba6cae39f386f8c84871860ba0c384d
cntencra <cn10cra@gmail.com>
2025-05-01 16:39:57 +0100

    feat: header updates

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import '../css/header.css'
+
 import { useContext } from "react"
 import { Link } from "react-router-dom"
 import { UserContext } from "../contexts/User"
@@ -6,20 +8,22 @@ const Header = () => {
     const { userName } = useContext(UserContext)
 
     return (
+        <>
         <div id="header-container">
             <Link to ='/'>
-            <div id="home-icon-container"><img src="http://i.imgur.com/i48gxxj.jpg" alt="Chris's News Logo" /></div>
+            <div id="home-icon-container"><img src="src/assets/news-icon.png" alt="Chris's News Logo" /></div>
             </Link>
             <h1>Chris's NC News</h1>
             <div id="header-logged-in-user">
                 <Link to='/user'>
-                <img src="https://www.svgrepo.com/show/335455/profile-default.svg" alt="user profile picture" />
-                    <p id="header-username">
-                        {userName}
-                    </p>
+                    <img src="https://www.svgrepo.com/show/335455/profile-default.svg" alt={`Logged in User ${userName}`} />
                 </Link>
+                <div className="hidden-user">{userName}</div>
             </div>
         </div>
+       
+        </>
+
     )
 
 }
