6196cf6a7b88d56dfb869e588293d58987d2f110
cntencra <cn10cra@gmail.com>
2025-05-05 12:39:48 +0100

    feat: Articles added className to btns

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -41,21 +41,21 @@ import Loading from './Loading';
             <div id="sort-by-container">
                 <div className="sort-dropdown">
                     <div className="sort-btn">
-                        <h3>sort by▼</h3>
+                        <h5>sort▼</h5>
                     </div>
                     <ul className="sort-dropdown-content">
-                        <li><button onClick={()=>{setSortType('created_at')}}>date</button></li>
-                        <li><button onClick={()=>{setSortType('comment_count')}}>comment count</button></li>
-                        <li><button onClick={()=>{setSortType('votes')}}>votes</button></li>
+                        <li><button className="dropdown-btn" onClick={()=>{setSortType('created_at')}}>Date</button></li>
+                        <li><button className="dropdown-btn" onClick={()=>{setSortType('comment_count')}}>Comments</button></li>
+                        <li><button className="dropdown-btn" onClick={()=>{setSortType('votes')}}>Popular</button></li>
                     </ul>
                 </div>
                 <div className="sort-dropdown">
                     <div className="sort-btn">
-                        <h3>order▼</h3>
+                        <h5>order▼</h5>
                     </div>
                     <ul className="sort-dropdown-content">
-                        <li><button onClick={()=>{setOrder('desc')}}>highest first</button></li>
-                        <li><button onClick={()=>{setOrder('asc')}}>lowest first</button></li>
+                        <li><button className="dropdown-btn" onClick={()=>{setOrder('desc')}}>Highest</button></li>
+                        <li><button className="dropdown-btn" onClick={()=>{setOrder('asc')}}>Lowest</button></li>
                     </ul>
                 </div>
             </div>
